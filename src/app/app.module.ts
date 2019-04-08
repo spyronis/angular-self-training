b2626e5de425fd567bcbc62b6edd35fc3843feb9
Giannis Spyronis <Giannis.Spyronis@arhs-dev-hellas.com>
2019-04-08 19:26:08 +0300

    Initial commit angular-js arhs training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BindingComponent } from './binding/binding.component';
+import { UserComponent } from './user/user.component';
+import { TestDirective } from './test.directive';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent,
+    UserComponent,
+    TestDirective,
+    TemplateFormsComponent,
+    ReactiveFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
